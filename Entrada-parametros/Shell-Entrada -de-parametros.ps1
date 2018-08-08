985a5139aa177fba3890f335a938ea4486a39fbe
leandro.alves <leandro.alves@s2it.com.br>
2018-08-08 08:55:51 -0300

    Exemplo entrada de parametros

---
+++ Entrada-parametros/Shell-Entrada -de-parametros.ps1
@@ -0,0 +1,12 @@
+﻿param 
+( 
+    [string]$Computer = "MyDefaultComputer", 
+    [string]$User = "MyDefaultUser"
+
+)
+
+$Computer
+
+$User
+
+#Execute na linha de comando shell: & '.\Shell-Entrada -de-parametros.ps1' -Computer Notebook -User Leandro
---
+++ Replace/Shell-Replace.ps1
@@ -0,0 +1,5 @@
+﻿$regex = "('asdf'|'jklç'),\s"
+$replace = ""
+(Get-Content .\texto.txt) -replace $regex, $replace  | Set-Content texto.txt
+
+
