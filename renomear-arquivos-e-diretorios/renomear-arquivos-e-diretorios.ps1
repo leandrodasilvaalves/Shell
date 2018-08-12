79cb902888389283fcde8c508988e67c6c9a9a44
Leandro Alves <leandro.silva.alves86@gmail.com>
2018-08-12 10:31:25 -0300

    renomear-arquivos-e-diretorios

---
+++ renomear-arquivos-e-diretorios/renomear-arquivos-e-diretorios.ps1
@@ -0,0 +1,12 @@
+﻿#referencia
+#http://underpop.online.fr/w/windows-power-shell/rename-item-microsoft-windows-powershell.htm
+
+cls
+$source = 'C:\Files'
+cd $source
+
+#Renomear arquivos
+Get-ChildItem *.* -Recurse | Rename-Item -NewName { $_.Name -replace '\s', '-'}
+
+#Renomear diretorios
+Get-ChildItem | Rename-Item -NewName { $_.Name -replace '\s', '-'}
