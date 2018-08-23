ca47fed1061c8aefbd7dbd9eb2a58b8169cd9e64
leandro.alves <leandro.alves@s2it.com.br>
2018-08-23 09:22:44 -0300

    Adicionado novo filtro

--- Filtrar-dados/script.ps1
+++
@@ -1,3 +0,0 @@
-#REFERÊNCIA: https://docs.microsoft.com/en-us/powershell/scripting/getting-started/cookbooks/removing-objects-from-the-pipeline--where-object-?view=powershell-6
-
-Get-ChildItem $env:USERPROFILE\desktop | Where-Object -FilterScript {($_.Name -like '*.lnk*')}
--- Filtrar-e-ordenar-dados/script.ps1
+++ Filtrar-e-ordenar-dados/script.ps1
@@ -5,3 +5,5 @@ Get-ChildItem $env:USERPROFILE\desktop | Where-Object -FilterScript {($_.Name -l
 $processo = Get-Process
 $processo | Sort-Object -Property ProcessName
 $winArray = $processo | Sort-Object -Property ProcessName | Where-Object -FilterScript {$_.ProcessName -like 'win*'}
+
+Get-ChildItem . -Recurse -Force | Where-Object -FilterScript {($_.LastWriteTime -gt '2018-06-01') -and ($_.Name -like 'Email*.cs')}
